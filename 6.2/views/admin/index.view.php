0962fbb00ba27e4cbe2b7fc53860e58cf2e27a78
duaneo2011 <duaneo2011@gmail.com>
2022-07-12 09:20:20 +0200

    Connected our application to a db
    Connected our application to a  sql db

---
+++ 6.2/views/admin/index.view.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="mt-5"></h1>
+        </div>
+        
+    </div>
+    <div class="row" style="margin-top:20px">
+        <a href="create.php">Create New Term</a>
+    </div>
+    <div class="row">
+        <table class="table table-striped">
+        <?php foreach ($model as $item) : ?>
+            <tr>
+                <td><a href="edit.php?key=<?= $item->term ?>">Edit</a></td>
+                <td><?= $item->term ?></td>
+                <td><?= $item->definition ?></td>
+                <td><a href="delete.php?key=<?= $item->term ?>">Delete</a></td>
+            </tr>
+        <?php endforeach; ?>
+    </div>
+</div>
